03cbc56ecbfec19a87da7a661b5ca44998ddea4c
torakiki <andrea.vacondio@gmail.com>
2011-07-10 10:38:44 +0200

    0000001: inherited test unit against cycles.

---
+++ sejda-console/src/test/java/org/sejda/TestCyclesCLI.java
@@ -0,0 +1,15 @@
+package org.sejda;
+
+import org.sejda.core.TestCycles;
+
+/**
+ * Test for cycles.
+ * 
+ * @author Andrea Vacondio
+ * 
+ */
+public class TestCyclesCLI extends TestCycles {
+
+    // inherited tests
+
+}
---
+++ sejda-itext/src/test/java/org/sejda/core/TestCyclesIText.java
@@ -0,0 +1,13 @@
+package org.sejda.core;
+
+/**
+ * Tests for cycles.
+ * 
+ * @author Andrea Vacondio
+ * 
+ */
+public class TestCyclesIText extends TestCycles {
+
+    // hinerited tests
+
+}
