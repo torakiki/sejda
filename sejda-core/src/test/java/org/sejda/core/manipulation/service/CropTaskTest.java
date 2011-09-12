83b35d279d1238fb55bcabd2ea6d1e3a4826816d
torakiki <andrea.vacondio@gmail.com>
2011-09-12 22:53:23 +0200

    0000035: implemented crop task with iText.

---
+++ sejda-core/src/test/java/org/sejda/core/manipulation/service/CropTaskTest.java
@@ -0,0 +1,114 @@
+/*
+ * Created on 10/set/2011
+ * Copyright 2011 by Andrea Vacondio (andrea.vacondio@gmail.com).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+package org.sejda.core.manipulation.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.awt.Point;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.sejda.core.TestUtils;
+import org.sejda.core.exception.TaskException;
+import org.sejda.core.manipulation.model.RectangularBox;
+import org.sejda.core.manipulation.model.input.PdfStreamSource;
+import org.sejda.core.manipulation.model.parameter.CropParameters;
+import org.sejda.core.manipulation.model.pdf.PdfVersion;
+import org.sejda.core.manipulation.model.task.Task;
+
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfReader;
+
+/**
+ * @author Andrea Vacondio
+ * 
+ */
+@Ignore
+public abstract class CropTaskTest extends PdfOutEnabledTest implements TestableTask<CropParameters> {
+
+    private static final RectangularBox EVEN_PAGES_RECTANGLE = RectangularBox.newInstanceFromPoints(new Point(0, 0),
+            new Point(595, 421));
+    private static final RectangularBox ODD_PAGES_RECTANGLE = RectangularBox.newInstanceFromPoints(new Point(0, 421),
+            new Point(595, 842));
+
+    private DefaultTaskExecutionService victim = new DefaultTaskExecutionService();
+
+    private TaskExecutionContext context = mock(DefaultTaskExecutionContext.class);
+    private CropParameters parameters;
+
+    @Before
+    public void setUp() {
+        setUpParameters();
+        TestUtils.setProperty(victim, "context", context);
+    }
+
+    /**
+     * Set up of the set page labels parameters
+     * 
+     */
+    private void setUpParameters() {
+        parameters = new CropParameters();
+        parameters.setCompress(false);
+        parameters.setVersion(PdfVersion.VERSION_1_6);
+        parameters.addCropArea(ODD_PAGES_RECTANGLE);
+        parameters.addCropArea(EVEN_PAGES_RECTANGLE);
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("pdf/test_file.pdf");
+        PdfStreamSource source = PdfStreamSource.newInstanceNoPassword(stream, "test_file.pdf");
+        parameters.setSource(source);
+        parameters.setOverwrite(true);
+    }
+
+    @Test
+    public void testExecute() throws TaskException, IOException {
+        when(context.getTask(parameters)).thenReturn((Task) getTask());
+        initializeNewFileOutput(parameters);
+        victim.execute(parameters);
+        PdfReader reader = getReaderFromResultFile();
+        assertCreator(reader);
+        assertVersion(reader, PdfVersion.VERSION_1_6);
+        assertEquals(8, reader.getNumberOfPages());
+        for (int i = 1; i <= reader.getNumberOfPages(); i = i + 2) {
+            Rectangle crop = reader.getBoxSize(i, "crop");
+            assertEqualsRectangleOddPages(ODD_PAGES_RECTANGLE, crop);
+            Rectangle media = reader.getBoxSize(i, "media");
+            assertEqualsRectangleOddPages(ODD_PAGES_RECTANGLE, media);
+        }
+        for (int i = 2; i <= reader.getNumberOfPages(); i = i + 2) {
+            Rectangle crop = reader.getBoxSize(i, "crop");
+            assertEqualsRectangleOddPages(EVEN_PAGES_RECTANGLE, crop);
+            Rectangle media = reader.getBoxSize(i, "media");
+            assertEqualsRectangleOddPages(EVEN_PAGES_RECTANGLE, media);
+        }
+        reader.close();
+    }
+
+    private void assertEqualsRectangleOddPages(RectangularBox expected, Rectangle found) {
+        assertEquals(expected.getLeft(), (int) found.getLeft());
+        assertEquals(expected.getBottom(), (int) found.getBottom());
+        assertEquals(expected.getRight(), (int) found.getRight());
+        assertEquals(expected.getTop(), (int) found.getTop());
+    }
+
+    protected CropParameters getParameters() {
+        return parameters;
+    }
+}
---
+++ sejda-itext/src/main/java/org/sejda/core/manipulation/model/task/itext/CropTask.java
@@ -0,0 +1,109 @@
+/*
+ * Created on 10/set/2011
+ * Copyright 2011 by Andrea Vacondio (andrea.vacondio@gmail.com).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+package org.sejda.core.manipulation.model.task.itext;
+
+import static org.sejda.core.manipulation.model.task.itext.component.PdfCopiers.nullSafeClosePdfCopy;
+import static org.sejda.core.manipulation.model.task.itext.util.ITextUtils.nullSafeClosePdfReader;
+import static org.sejda.core.notification.dsl.ApplicationEventsNotifier.notifyEvent;
+import static org.sejda.core.support.io.model.FileOutput.file;
+
+import java.io.File;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.sejda.core.exception.TaskException;
+import org.sejda.core.manipulation.model.RectangularBox;
+import org.sejda.core.manipulation.model.input.PdfSource;
+import org.sejda.core.manipulation.model.input.PdfSourceOpener;
+import org.sejda.core.manipulation.model.parameter.CropParameters;
+import org.sejda.core.manipulation.model.task.Task;
+import org.sejda.core.manipulation.model.task.itext.component.DefaultPdfCopier;
+import org.sejda.core.manipulation.model.task.itext.component.input.PdfSourceOpeners;
+import org.sejda.core.support.io.SingleOutputWriterSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.lowagie.text.pdf.PdfDictionary;
+import com.lowagie.text.pdf.PdfName;
+import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.PdfRectangle;
+
+/**
+ * iText implementation of the Crop task to set MEDIABOX and CROPBOX on an input document. This task allow multiple boxes on the same page, generating an output document that
+ * contains duplicated pages with different boxes applied.
+ * 
+ * @author Andrea Vacondio
+ * 
+ */
+public class CropTask implements Task<CropParameters> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CropTask.class);
+
+    private PdfReader reader = null;
+    private DefaultPdfCopier copier = null;
+    private SingleOutputWriterSupport outputWriter;
+    private PdfSourceOpener<PdfReader> sourceOpener;
+
+    public void before(CropParameters parameters) {
+        outputWriter = new SingleOutputWriterSupport();
+        sourceOpener = PdfSourceOpeners.newFullReadOpener();
+    }
+
+    public void execute(CropParameters parameters) throws TaskException {
+        PdfSource source = parameters.getSource();
+        LOG.debug("Opening {} ...", source);
+        reader = source.open(sourceOpener);
+        int totalPages = reader.getNumberOfPages();
+
+        File tmpFile = outputWriter.createTemporaryPdfBuffer();
+        LOG.debug("Created output temporary buffer {} ...", tmpFile);
+
+        copier = new DefaultPdfCopier(reader, tmpFile, parameters.getVersion());
+        copier.setCompression(parameters.isCompressXref());
+
+        Set<PdfRectangle> cropAreas = getPdfRectangles(parameters.getCropAreas());
+        for (int page = 1; page <= totalPages; page++) {
+            PdfDictionary dictionary = reader.getPageN(page);
+            for (PdfRectangle cropBox : cropAreas) {
+                LOG.trace("Applying crop box {} to page {}", cropBox, page);
+                dictionary.put(PdfName.MEDIABOX, cropBox);
+                dictionary.put(PdfName.CROPBOX, cropBox);
+                copier.addPage(reader, page);
+            }
+            notifyEvent().stepsCompleted(page).outOf(totalPages);
+        }
+        nullSafeClosePdfCopy(copier);
+        nullSafeClosePdfReader(reader);
+
+        outputWriter.flushSingleOutput(file(tmpFile).name(parameters.getOutputName()), parameters.getOutput(),
+                parameters.isOverwrite());
+        LOG.debug("Crop areas applied to {}", parameters.getOutput());
+    }
+
+    public void after() {
+        nullSafeClosePdfCopy(copier);
+        nullSafeClosePdfReader(reader);
+    }
+
+    private Set<PdfRectangle> getPdfRectangles(Set<RectangularBox> areas) {
+        Set<PdfRectangle> retVal = new LinkedHashSet<PdfRectangle>();
+        for (RectangularBox box : areas) {
+            retVal.add(new PdfRectangle(box.getLeft(), box.getBottom(), box.getRight(), box.getTop()));
+        }
+        return retVal;
+    }
+}
