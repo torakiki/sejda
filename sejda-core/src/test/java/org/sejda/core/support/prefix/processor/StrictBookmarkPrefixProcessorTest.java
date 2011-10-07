ccb7e647c999f371aad2bd062e4298e986bd8de6
torakiki <andrea.vacondio@gmail.com>
2011-10-07 11:03:39 +0200

    0000032: added a stricter BOOKMARK_NAME prefix in which anything that is not A-Z0-9_ is trimmed.

---
+++ sejda-core/src/test/java/org/sejda/core/support/prefix/processor/StrictBookmarkPrefixProcessorTest.java
@@ -0,0 +1,51 @@
+/*
+ * Created on 07/ott/2011
+ * Copyright 2011 by Andrea Vacondio (andrea.vacondio@gmail.com).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+package org.sejda.core.support.prefix.processor;
+
+import static org.junit.Assert.assertEquals;
+import static org.sejda.core.support.prefix.model.NameGenerationRequest.nameRequest;
+
+import org.junit.Test;
+
+/**
+ * @author Andrea Vacondio
+ * 
+ */
+public class StrictBookmarkPrefixProcessorTest extends BasePrefixProcessorTest {
+    private StrictBookmarkPrefixProcessor victim = new StrictBookmarkPrefixProcessor();
+
+    @Override
+    public PrefixProcessor getProcessor() {
+        return victim;
+    }
+
+    @Test
+    public void testComplexProcess() {
+        String prefix = "prefix_[BOOKMARK_NAME_STRICT]_[BASENAME]";
+        String bookmark = "book";
+        String expected = "prefix_book_[BASENAME]";
+        assertEquals(expected, victim.process(prefix, nameRequest().bookmark(bookmark)));
+    }
+
+    @Test
+    public void testComplexProcessInvalidChars() {
+        String prefix = "prefix_[BOOKMARK_NAME_STRICT]_[BASENAME]";
+        String bookmark = "book<>?$ç°";
+        String expected = "prefix_book_[BASENAME]";
+        assertEquals(expected, victim.process(prefix, nameRequest().bookmark(bookmark)));
+    }
+}
