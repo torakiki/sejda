f1702d62a3fe0a80b2bdd9382f331560f001a63c
ediweissmann <edi.weissmann@gmail.com>
2011-10-18 00:51:51 +0200

    fix smoke tests script for cygwin/windows, clean up duplicated smoke test commands

--- sejda-console/src/test/shell/smoke-tests.sh
+++ sejda-console/src/test/shell/smoke-tests.sh
@@ -9,10 +9,18 @@ BASEDIR=`(cd "$BASEDIR"; pwd)`
 
 echo "Adding execute permission to the console binaries"
 chmod +x $BASEDIR/target/assembled/bin/sejda-console
+
 echo "Creating /tmp/sejda-smoketest folder if it doesnt exist"
 mkdir -p /tmp/sejda-smoketest
+
 echo "Removing previous files from /tmp/sejda-smoketest"
 rm -fr /tmp/sejda-smoketest/*
+
+# cygwin on windows
+if [ -d "/cygdrive/c/tmp/sejda-smoketest/" ]; then
+    echo "Removing previous files from /cygdrive/c/tmp/sejda-smoketest"
+	rm -fr /cygdrive/c/tmp/sejda-smoketest/*
+fi
 
 function run_tests {
   local exit_code=0
