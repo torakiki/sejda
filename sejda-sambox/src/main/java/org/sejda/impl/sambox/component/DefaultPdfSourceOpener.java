247528fc5903d6893ebe864d01dae1dd9eb05875
Andrea Vacondio <andrea.vacondio@gmail.com>
2015-09-17 16:51:00 +0200

    close #191: throw TaskWrongPasswordException if the password is wrong

--- sejda-sambox/src/main/java/org/sejda/impl/sambox/component/DefaultPdfSourceOpener.java
+++ sejda-sambox/src/main/java/org/sejda/impl/sambox/component/DefaultPdfSourceOpener.java
@@ -21,12 +21,14 @@ import java.io.IOException;
 
 import org.sejda.io.SeekableSources;
 import org.sejda.model.exception.TaskIOException;
+import org.sejda.model.exception.TaskWrongPasswordException;
 import org.sejda.model.input.PdfFileSource;
 import org.sejda.model.input.PdfSourceOpener;
 import org.sejda.model.input.PdfStreamSource;
 import org.sejda.model.input.PdfURLSource;
 import org.sejda.sambox.input.PDFParser;
 import org.sejda.sambox.pdmodel.PDDocument;
+import org.sejda.sambox.pdmodel.encryption.InvalidPasswordException;
 
 /**
  * SAMBox component able to open a PdfSource and return the corresponding {@link PDDocumentHandler}.
@@ -36,33 +38,46 @@ import org.sejda.sambox.pdmodel.PDDocument;
  */
 public class DefaultPdfSourceOpener implements PdfSourceOpener<PDDocumentHandler> {
 
+    private static final String WRONG_PWD_MESSAGE = "Unable to open the document due to a wrong password.";
+    private static final String ERROR_MESSAGE = "An error occurred opening the source: %s.";
+
     @Override
     public PDDocumentHandler open(PdfURLSource source) throws TaskIOException {
         try {
-            PDDocument document = PDFParser.parse(SeekableSources.onTempFileSeekableSourceFrom(source.getSource().openStream()), source.getPassword());
+            PDDocument document = PDFParser.parse(
+                    SeekableSources.onTempFileSeekableSourceFrom(source.getSource().openStream()),
+                    source.getPassword());
             return new PDDocumentHandler(document);
+        } catch (InvalidPasswordException ipe) {
+            throw new TaskWrongPasswordException(WRONG_PWD_MESSAGE, ipe);
         } catch (IOException e) {
-            throw new TaskIOException(String.format("An error occurred opening the source: %s.", source), e);
+            throw new TaskIOException(String.format(ERROR_MESSAGE, source), e);
         }
     }
 
     @Override
     public PDDocumentHandler open(PdfFileSource source) throws TaskIOException {
         try {
-            PDDocument document = PDFParser.parse(SeekableSources.seekableSourceFrom(source.getSource()), source.getPassword());
+            PDDocument document = PDFParser.parse(SeekableSources.seekableSourceFrom(source.getSource()),
+                    source.getPassword());
             return new PDDocumentHandler(document);
+        } catch (InvalidPasswordException ipe) {
+            throw new TaskWrongPasswordException(WRONG_PWD_MESSAGE, ipe);
         } catch (IOException e) {
-            throw new TaskIOException(String.format("An error occurred opening the source: %s.", source), e);
+            throw new TaskIOException(String.format(ERROR_MESSAGE, source), e);
         }
     }
 
     @Override
     public PDDocumentHandler open(PdfStreamSource source) throws TaskIOException {
         try {
-            PDDocument document = PDFParser.parse(SeekableSources.onTempFileSeekableSourceFrom(source.getSource()), source.getPassword());
+            PDDocument document = PDFParser.parse(SeekableSources.onTempFileSeekableSourceFrom(source.getSource()),
+                    source.getPassword());
             return new PDDocumentHandler(document);
+        } catch (InvalidPasswordException ipe) {
+            throw new TaskWrongPasswordException(WRONG_PWD_MESSAGE, ipe);
         } catch (IOException e) {
-            throw new TaskIOException(String.format("An error occurred opening the source: %s.", source), e);
+            throw new TaskIOException(String.format(ERROR_MESSAGE, source), e);
         }
     }
 }
---
+++ sejda-sambox/src/test/java/org/sejda/impl/sambox/component/DefaultPdfSourceOpenerTest.java
@@ -0,0 +1,58 @@
+/*
+ * Created on 17 set 2015
+ * Copyright 2015 by Andrea Vacondio (andrea.vacondio@gmail.com).
+ * This file is part of Sejda.
+ *
+ * Sejda is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sejda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Sejda.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sejda.impl.sambox.component;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.sejda.model.exception.TaskIOException;
+import org.sejda.model.exception.TaskWrongPasswordException;
+import org.sejda.model.input.PdfStreamSource;
+
+/**
+ * @author Andrea Vacondio
+ *
+ */
+public class DefaultPdfSourceOpenerTest {
+
+    @Test
+    public void openDocument() throws TaskIOException {
+        assertNotNull(new DefaultPdfSourceOpener().open(PdfStreamSource.newInstanceNoPassword(
+                getClass().getClassLoader().getResourceAsStream("pdf/test_file.pdf"), "my source")));
+    }
+
+    @Test
+    public void openDocumentWithPassword() throws TaskIOException {
+        assertNotNull(new DefaultPdfSourceOpener().open(PdfStreamSource.newInstanceWithPassword(
+                getClass().getClassLoader().getResourceAsStream("pdf/test-enc.pdf"), "my source", "test")));
+    }
+
+    @Test(expected = TaskWrongPasswordException.class)
+    public void openDocumentWrongPassword() throws TaskIOException {
+        new DefaultPdfSourceOpener().open(PdfStreamSource.newInstanceNoPassword(
+                getClass().getClassLoader().getResourceAsStream("pdf/test-enc.pdf"), "my source"));
+    }
+
+    @Test(expected = TaskIOException.class)
+    public void openDocumentError() throws TaskIOException {
+        new DefaultPdfSourceOpener().open(PdfStreamSource.newInstanceNoPassword(
+                getClass().getClassLoader().getResourceAsStream("pdf/not_a_pdf.pdf"), "my source"));
+
+    }
+}
