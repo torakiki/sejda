3c24a9ca3f99af6106842243e995116c8e603863
Andrea Vacondio <andrea.vacondio@gmail.com>
2016-03-07 11:38:05 +0100

    Moved the footer a little down

--- sejda-sambox/src/main/java/org/sejda/impl/sambox/component/HeaderFooterWriter.java
+++ sejda-sambox/src/main/java/org/sejda/impl/sambox/component/HeaderFooterWriter.java
@@ -80,7 +80,7 @@ public class HeaderFooterWriter {
         try {
             float stringWidth = latestSuitablefont.getStringWidth(label) * fontSize.floatValue() / 1000f;
             Point2D position = new Point2D.Float(hAlign.position(pageSize.getWidth(), stringWidth, DEFAULT_MARGIN),
-                    vAlign.position(pageSize.getHeight(), DEFAULT_MARGIN));
+                    vAlign.position(pageSize.getHeight(), DEFAULT_MARGIN - fontSize.floatValue()));
             try (PDPageContentStream contentStream = new PDPageContentStream(document, page, true, true)) {
                 contentStream.beginText();
                 contentStream.setFont(latestSuitablefont, fontSize.floatValue());
