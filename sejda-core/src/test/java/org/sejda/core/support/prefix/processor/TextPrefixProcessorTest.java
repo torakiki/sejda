7860a859ae48106d9a70571ebfb75217579541d0
Andrea Vacondio <andrea.vacondio@gmail.com>
2016-06-21 18:50:23 +0200

    Delete test file after creation

--- sejda-core/src/test/java/org/sejda/core/support/prefix/processor/TextPrefixProcessorTest.java
+++ sejda-core/src/test/java/org/sejda/core/support/prefix/processor/TextPrefixProcessorTest.java
@@ -1,13 +1,13 @@
 package org.sejda.core.support.prefix.processor;
 
-import org.junit.Test;
-import org.sejda.core.support.prefix.model.NameGenerationRequest;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.sejda.core.support.prefix.model.NameGenerationRequest;
 
 public class TextPrefixProcessorTest {
 
@@ -20,5 +20,6 @@ public class TextPrefixProcessorTest {
 
         File file = File.createTempFile(actual, ".pdf");
         assertTrue(file.exists());
+        file.delete();
     }
 }
