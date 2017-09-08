e6ec5af1f22e16016a978608d25fca915b092de7
Edi Weissmann <edi.weissmann@gmail.com>
2017-09-08 10:24:43 +0200

    Adds name to PageSize

--- sejda-model/src/main/java/org/sejda/model/parameter/PageSize.java
+++ sejda-model/src/main/java/org/sejda/model/parameter/PageSize.java
@@ -25,6 +25,7 @@ public class PageSize {
 
     private float width;
     private float height;
+    private String name;
 
     /** user space units per inch */
     private static final float POINTS_PER_INCH = 72;
@@ -34,34 +35,39 @@ public class PageSize {
 
     /** A rectangle the size of U.S. Letter, 8.5" x 11". */
     public static final PageSize LETTER = new PageSize(8.5f * POINTS_PER_INCH,
-            11f * POINTS_PER_INCH);
+            11f * POINTS_PER_INCH, "Letter");
     /** A rectangle the size of U.S. Legal, 8.5" x 14". */
     public static final PageSize LEGAL = new PageSize(8.5f * POINTS_PER_INCH,
-            14f * POINTS_PER_INCH);
+            14f * POINTS_PER_INCH, "Legal");
     /** A rectangle the size of A0 Paper. */
-    public static final PageSize A0 = new PageSize(841 * POINTS_PER_MM, 1189 * POINTS_PER_MM);
+    public static final PageSize A0 = new PageSize(841 * POINTS_PER_MM, 1189 * POINTS_PER_MM, "A0");
 
     /** A rectangle the size of A1 Paper. */
-    public static final PageSize A1 = new PageSize(594 * POINTS_PER_MM, 841 * POINTS_PER_MM);
+    public static final PageSize A1 = new PageSize(594 * POINTS_PER_MM, 841 * POINTS_PER_MM, "A1");
 
     /** A rectangle the size of A2 Paper. */
-    public static final PageSize A2 = new PageSize(420 * POINTS_PER_MM, 594 * POINTS_PER_MM);
+    public static final PageSize A2 = new PageSize(420 * POINTS_PER_MM, 594 * POINTS_PER_MM, "A2");
 
     /** A rectangle the size of A3 Paper. */
-    public static final PageSize A3 = new PageSize(297 * POINTS_PER_MM, 420 * POINTS_PER_MM);
+    public static final PageSize A3 = new PageSize(297 * POINTS_PER_MM, 420 * POINTS_PER_MM, "A3");
 
     /** A rectangle the size of A4 Paper. */
-    public static final PageSize A4 = new PageSize(210 * POINTS_PER_MM, 297 * POINTS_PER_MM);
+    public static final PageSize A4 = new PageSize(210 * POINTS_PER_MM, 297 * POINTS_PER_MM, "A4");
 
     /** A rectangle the size of A5 Paper. */
-    public static final PageSize A5 = new PageSize(148 * POINTS_PER_MM, 210 * POINTS_PER_MM);
+    public static final PageSize A5 = new PageSize(148 * POINTS_PER_MM, 210 * POINTS_PER_MM, "A5");
 
     /** A rectangle the size of A6 Paper. */
-    public static final PageSize A6 = new PageSize(105 * POINTS_PER_MM, 148 * POINTS_PER_MM);
+    public static final PageSize A6 = new PageSize(105 * POINTS_PER_MM, 148 * POINTS_PER_MM, "A6");
 
-    public PageSize(float width, float height) {
+    public PageSize(float width, float height, String name) {
         this.width = width;
         this.height = height;
+        this.name = name;
+    }
+
+    public PageSize(float width, float height) {
+        this(width, height, null);
     }
 
     public float getWidth() {
@@ -108,5 +114,9 @@ public class PageSize {
 
     public static PageSize fromInches(float widthInch, float heightInch) {
         return new PageSize(widthInch * POINTS_PER_INCH, heightInch * POINTS_PER_INCH);
+    }
+
+    public String getName() {
+        return name;
     }
 }
