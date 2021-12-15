e538b9eeeeab8bc65fc1b4b0e1eb5dcc5272a948
torakiki <andrea.vacondio@gmail.com>
2021-12-15 09:44:23 +0100

    upgraded dependencies and maven plugins versions

--- sejda-model/src/test/java/org/sejda/model/validation/validator/PdfVersionValidatorTest.java
+++ sejda-model/src/test/java/org/sejda/model/validation/validator/PdfVersionValidatorTest.java
@@ -21,7 +21,7 @@ package org.sejda.model.validation.validator;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
